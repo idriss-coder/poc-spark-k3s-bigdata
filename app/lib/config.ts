47d52692412aa202a92cb8080d1795da42506b8a
Idriss duval <94405855+idriss-coder@users.noreply.github.com>
2026-03-31 15:48:39 +0100

    Refactor API and cluster monitor URLs to use BASE_IP

--- app/lib/config.ts
+++ app/lib/config.ts
@@ -1,7 +1,9 @@
+CONST BASE_IP = "http://3.91.241.127",
+
 const CONFIG = {
-  API_URL: "http://3.91.241.127",
+  API_URL: BASE_IP,
   //API_URL: "http://localhost:8000",
-  CLUSTER_MONITOR_URL: "http://13.221.79.180:8765/cluster", 
+  CLUSTER_MONITOR_URL: `${BASE_IP}:8765/cluster`, 
 }
 
 export default CONFIG;
